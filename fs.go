e200775fece2db48f4f13c60caa53816d7185c83
rwxrob <rob@rwx.gg>
2022-03-22 06:36:11 -0400

    Add DupPerms

--- fs.go
+++ fs.go
@@ -15,3 +15,16 @@ func IsDir(path string) bool {
 	}
 	return info.IsDir()
 }
+
+// DupPerms duplicates the perms of one file or directory onto the second.
+func DupPerms(orig, clone string) error {
+	ostats, err := os.Stat(orig)
+	if err != nil {
+		return err
+	}
+	_, err = os.Stat(clone)
+	if err != nil {
+		return nil
+	}
+	return os.Chmod(clone, ostats.Mode())
+}
--- fs_test.go
+++ fs_test.go
@@ -2,6 +2,7 @@ package fs_test
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/rwxrob/fs"
 )
@@ -12,4 +13,23 @@ func ExampleIsDir() {
 	// Output:
 	// true
 	// false
+}
+
+func ExampleDupPerms() {
+	os.Mkdir(`testdata/some`, 0000)
+	defer os.Remove(`testdata/some`)
+	stats, _ := os.Stat(`testdata/orig`)
+	fmt.Println(stats.Mode())
+	stats, _ = os.Stat(`testdata/some`)
+	fmt.Println(stats.Mode())
+	err := fs.DupPerms(`testdata/orig`, `testdata/some`)
+	if err != nil {
+		fmt.Println(err)
+	}
+	stats, _ = os.Stat(`testdata/some`)
+	fmt.Println(stats.Mode())
+	// Output:
+	// drw-------
+	// d---------
+	// drw-------
 }
